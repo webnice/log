b392b98477d48254ab0b047ed79edd59fa74b814
Alex Geer <monoflash@gmail.com>
2018-06-14 19:40:37 +0300

    Bugfix for syslog receiver build on windows

---
+++ receiver/syslog/syslog_windows.go
@@ -0,0 +1,40 @@
+// +build windows
+
+package syslog
+
+import (
+	"fmt"
+	"os"
+
+	s "gopkg.in/webnice/log.v2/sender"
+)
+
+// Interface is an interface of package
+type Interface interface {
+	// Receiver Message receiver
+	Receiver(s.Message)
+
+	// SetAddress Назначение адреса syslog сервера
+	SetAddress(string, string) Interface
+}
+
+// impl is an implementation of package
+type impl struct {
+}
+
+// New Create new
+func New() Interface {
+	var rcv = new(impl)
+	return rcv
+}
+
+// SetAddress Назначение адреса syslog сервера
+func (rcv *impl) SetAddress(proto string, address string) Interface {
+	fmt.Fprintf(os.Stderr, "Syslog not implemented on windows platform\n")
+	return rcv
+}
+
+// Receiver Message receiver. Output to Syslog
+func (rcv *impl) Receiver(msg s.Message) {
+	fmt.Fprintf(os.Stderr, "Syslog not implemented on windows platform\n")
+}
